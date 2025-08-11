6d903b51c754f39d63f7d50d4dc5dd492289fb87
Mamadu Sama <mamadusama19@gmail.com>
2025-08-11 23:03:21 +0100

    feat add: initial pages (contacts, services,  about and cars)

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about,name='about'),
+    path("sevices", views.services, name="services"),
+    path("contacts", views.contacts, name="contacts"),
+    path("cars", views.cars, name="cars"),
+    
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,20 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'pages/home.html')
+
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def services(request):
+    return render(request, "pages/services.html")
+
+
+def contacts(request):
+    return render(request, "pages/contacts.html")
+
+
+def cars(request):
+    return render(request, "pages/cars.html")
