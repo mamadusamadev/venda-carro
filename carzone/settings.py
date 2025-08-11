c2f931801c66b76048dade26a355460fb8e7bc9f
Mamadu Sama <mamadusama19@gmail.com>
2025-08-12 00:35:07 +0100

    fix: database name erro in setting

--- carzone/settings.py
+++ carzone/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'venda_carro',
         'USER': 'postgres',
         'PASSWORD': 'RaiyanSama',
